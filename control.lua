cf3156783b82f25c2f5d22a4b43fe33601721c29
justarandomgeek <justarandomgeek@gmail.com>
2024-12-30 17:16:06 -0500

    2.0

--- control.lua
+++ control.lua
@@ -1,10 +1,12 @@
 script.on_event(defines.events.on_player_selected_area, function(event)
   if event.item == "wirestripper-tool" then
-    --game.print(serpent.block(event))
     -- remove all copper
     for _,ent in pairs(event.entities) do
       if ent.type == "electric-pole" then
-          ent.disconnect_neighbour()
+        local c = ent.get_wire_connector(defines.wire_connector_id.pole_copper, false)
+        if c then
+          c.disconnect_all(defines.wire_origin.player)
+        end
       end
     end
   end
@@ -12,12 +14,18 @@ end)
 
 script.on_event(defines.events.on_player_alt_selected_area, function(event)
   if event.item == "wirestripper-tool" then
-    --game.print(serpent.block(event))
     -- remove all red/green
     for _,ent in pairs(event.entities) do
       if ent.type == "electric-pole" then
-          ent.disconnect_neighbour(defines.wire_type.red)
-          ent.disconnect_neighbour(defines.wire_type.green)
+        local red = ent.get_wire_connector(defines.wire_connector_id.circuit_red, false)
+        if red then
+          red.disconnect_all(defines.wire_origin.player)
+        end
+
+        local green = ent.get_wire_connector(defines.wire_connector_id.circuit_green, false)
+        if green then
+          green.disconnect_all(defines.wire_origin.player)
+        end
       end
     end
   end
--- data.lua
+++ data.lua
@@ -8,23 +8,35 @@ data:extend({
     stack_size = 1,
     subgroup = "terrain",
     order = "d[wirestripper-tool]-a[plain]",
-    flags = {"goes-to-quickbar"},
-    selection_color = {r = 0.2, g = 0.8, b = 0.2, a = 0.2},
-    alt_selection_color = {r = 0.2, g = 0.8, b = 0.2, a = 0.2},
-    selection_mode = {"blueprint"},
-    alt_selection_mode = {"blueprint"},
-    selection_cursor_box_type = "copy",
-    alt_selection_cursor_box_type = "copy"
-  },
-  {
-    type = "recipe",
-    name = "wirestripper-tool",
-    enabled = true,
-    energy_required = 0.5,
-    ingredients =
-    {
-      {"electronic-circuit", 1}
+    flags = {"spawnable", "only-in-cursor"},
+    select = {
+      border_color = {r = 0.2, g = 0.8, b = 0.2, a = 0.2},
+      mode = "blueprint",
+      cursor_box_type = "electricity", -- copy?
+      entity_type_filters = {
+        "electric-pole"
+      }
     },
-    result = "wirestripper-tool"
-  }
+    alt_select = {
+      border_color = {r = 0.2, g = 0.8, b = 0.2, a = 0.2},
+      mode = "blueprint",
+      cursor_box_type = "electricity",
+      entity_type_filters = {
+        "electric-pole"
+      }
+    },
+  }--[[@as data.SelectionToolPrototype]],
+  {
+    type = "shortcut",
+    name = "give-wirestripper",
+    order = "b[blueprints]-g[blueprint]",
+    action = "spawn-item",
+    localised_name = {"shortcut.make-blueprint"},
+    item_to_spawn = "wirestripper-tool",
+    style = "red",
+    icon = "__wirestripper__/wirestripper.png",
+    icon_size = 32,
+    small_icon = "__wirestripper__/wirestripper.png",
+    small_icon_size = 32,
+  }--[[@as data.ShortcutPrototype]],
 })
